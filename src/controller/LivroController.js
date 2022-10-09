79ac50ffc2f276aaa13abccd4637633e4f830a4c
daniel <dani.ajala@yahoo.com>
2022-10-09 16:09:01 -0300

    crud-completo

--- src/controller/LivroController.js
+++ src/controller/LivroController.js
@@ -11,6 +11,18 @@ class LivroController {
     }
 
 
+    static listarId = (req,res)=>{
+
+        const id = req.params.id
+        livros.findById(id,(err,livros)=>{
+            if (err){
+                res.status(400).send({message:`${err.message} - id não existe`})
+            }else{
+                res.status(200).send(livros)
+            }
+        })
+    }
+
     static criar = (req,res)=>{
         let livro = new livros(req.body)
         livro.save((err)=>{
@@ -18,6 +30,29 @@ class LivroController {
                 res.status(500).send({message:`${err.message}- falha ao cadastrar o livro`})
             }else{
                 res.status(201).send(livro.toJSON())
+            }
+        })
+    }
+
+    static atualizar =(req,res)=>{
+
+        const id = req.params.id
+        livros.findByIdAndUpdate(id , {$set:req.body}, (err)=>{
+            if(!err){
+                res.status(200).send({message:"livro atualizado com sucesso"})
+            }else{
+                res.status(500).send({message:err.message})
+            }
+        })
+    }
+
+    static deletar =(req,res)=>{
+        const id = req.params.id
+        livros.findByIdAndRemove(id,(err)=>{
+            if (! err){
+                res.status(200).send({message:"livro excluido"})
+            }else{
+                res.status(500).send({message:err})
             }
         })
     }
--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -8,7 +8,10 @@ const router = express.Router();
 
 router
 .get("/livros",LivroController.listar)
+.get('/livros/:id',LivroController.listarId)
 .post('/livros',LivroController.criar)
+.put("/livros/:id",LivroController.atualizar)
+.delete("/livros/:id",LivroController.deletar)
 
 
 export default router
